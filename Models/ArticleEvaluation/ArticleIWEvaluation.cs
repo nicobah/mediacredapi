552ca01ebfa9a12e078c3b29a63bc4235aae076c
Aletho95 <altho18@student.sdu.dk>
2023-03-17 15:36:19 +0100

    Edited IW score calculation

--- Models/ArticleEvaluation/ArticleIWEvaluation.cs
+++ Models/ArticleEvaluation/ArticleIWEvaluation.cs
@@ -12,7 +12,7 @@
             //Calculates the score based on the number of IWs.
             //Each IW lowers the score by 10% and having any IWs subtracts another 20%
             //This means that the first IW will cost 30% and then 10% more per IW.
-            var resultCalc = iwCount > 0 ? 100 - 20 - (10*iwCount) : 100;
+            var resultCalc = iwCount > 0 ? (100 - 20) * (Math.Pow(0.9,(double)iwCount)) : 100;
             //To-do: Take 10% of the current value for each subsequent IW.
 
             return resultCalc >= 0 ? (double)resultCalc : 0.0;
