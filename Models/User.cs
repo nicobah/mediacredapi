5a8d4b4d67b96e9ca86b3a13bef17e1b270eb9e9
nicobah <42782542+nicobah@users.noreply.github.com>
2023-03-29 09:52:18 +0200

    refactored user

--- Models/User.cs
+++ Models/User.cs
@@ -5,7 +5,13 @@
         public int ID { get; set; }
         public string Name { get; set; }
         public List<Article> SubscribesTo { get; set; }
-        public UserSettings UserSettings { get; set; }
+        public double NotificationThreshold { get; set; }
+        public double InformationWeight { get; set; }
+        public double InappropriateWordsWeight { get; set; }
+        public double ReferencesWeight { get; set; }
+        public double TopicWeight { get; set; }
+        public double AuthorWeight { get; set; }
+        public double BackingsWeight { get; set; }
 
     }
 }
