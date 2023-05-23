4570edfe0ac7bd761c1cfb00c8f811b6280f0399
nicobah <42782542+nicobah@users.noreply.github.com>
2023-05-23 12:01:40 +0200

    addauth

---
+++ Models/AddAuthDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MediaCred.Models
+{
+    public class AddAuthDto
+    {
+        public string link { get; set; }
+        public string authorId { get; set; }
+    }
+}
--- Models/Services/QueryService.cs
+++ Models/Services/QueryService.cs
@@ -90,16 +90,12 @@ namespace MediaCred.Models.Services
                 return await GetArticleFromResult(results, resultAuthors, resultUsedAsBacking, resultArguments);
             }
 
-
             return null;
         }
 
         public async Task<List<Argument>> GetArgumentsByArticleLink(string url, string? userID)
         {
-            if(url.Last() == '/')
-            {
-                url = url.Substring(0, url.Length - 1);
-            }
+           
             var query = @"MATCH (art:Article{link:$url})-[:CLAIMS]->(arg)
                             return arg";
 
